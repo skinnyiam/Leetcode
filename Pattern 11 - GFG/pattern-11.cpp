9e43f9a7ebeba3947664882700669a1f1cbe3184
Saurabh Shukla <84450605+skinnyiam@users.noreply.github.com>
2023-02-28 21:35:46 +0530

    Added solution - LeetHub

---
+++ Pattern 11 - GFG/pattern-11.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i=0;i<n;i++){
+           if(i%2!=0){
+               for(int j=0;j<=i;j++){
+                   if(j%2!=0){
+                       cout<<"1"<<" ";
+                   }else{
+                       cout<<"0"<<" ";
+                   }
+                   
+               }
+               cout<<endl;
+           }
+           
+           else{
+               for(int j=0;j<=i;j++){
+                   if(j%2!=0){
+                       cout<<"0"<<" ";
+                   }else{
+                       cout<<"1"<<" ";
+                   }
+                   
+               }
+               cout<<endl;
+           }
+           
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
